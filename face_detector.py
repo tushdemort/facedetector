d8db80795b13397a421c8d4f8f3f9de9847d4480
tushdemort <40672760+tushdemort@users.noreply.github.com>
2021-10-09 10:12:06 +0530

    Add files via upload

---
+++ face_detector.py
@@ -0,0 +1,27 @@
+
+
+import cv2
+
+
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+
+
+webcam = cv2.VideoCapture(0)
+
+while True:
+	successful_frame_read, frame=webcam.read()
+
+	grayscaled_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+	face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+	for (x,y,w,h) in face_coordinates:
+		cv2.rectangle(frame,(x,y),(x+w,y+h) ,(0,0,256, 5))
+	cv2.imshow('Dace detector', frame)
+	key=cv2.waitKey(1)
+	if key==81 or key==113:
+		break
+
+
+
+print("code completed")
+
