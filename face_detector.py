6fc19ba24f6505363cfc5ae98a6703bf48798df2
tushdemort <40672760+tushdemort@users.noreply.github.com>
2021-10-22 07:13:42 +0530

    Update face_detector.py

--- face_detector.py
+++ face_detector.py
@@ -16,7 +16,7 @@ while True:
 	face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 	for (x,y,w,h) in face_coordinates:
 		cv2.rectangle(frame,(x,y),(x+w,y+h) ,(0,0,256, 5))
-	cv2.imshow('Dace detector', frame)
+	cv2.imshow('Face detector', frame)
 	key=cv2.waitKey(1)
 	if key==81 or key==113:
 		break
