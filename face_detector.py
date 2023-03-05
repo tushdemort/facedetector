56a3d3022a16481efa8a2a5e1d2d46397a660a44
tushdemort <40672760+tushdemort@users.noreply.github.com>
2023-03-05 21:19:03 +0530

    removed extra white spaces

--- face_detector.py
+++ face_detector.py
@@ -1,17 +1,10 @@
-
-
 import cv2
 
-
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-
-
 webcam = cv2.VideoCapture(0)
 
 while True:
 	successful_frame_read, frame=webcam.read()
-
 	grayscaled_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 	face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
 	for (x,y,w,h) in face_coordinates:
@@ -20,8 +13,6 @@ while True:
 	key=cv2.waitKey(1)
 	if key==81 or key==113:
 		break
-
-
-
+		
 print("code completed")
 
